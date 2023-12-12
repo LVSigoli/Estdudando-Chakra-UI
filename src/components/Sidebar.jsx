a7c9c1704317a44067dfffc73397030659fbddc7
Lucas Sigoli <lucasviniciussigoli@gmail.com>
2023-12-12 13:36:09 -0300

    feat: Add icons to sidebar elements

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,17 +1,27 @@
-import { List, ListItem } from "@chakra-ui/react";
+import { AtSignIcon, CalendarIcon, EditIcon } from "@chakra-ui/icons";
+import { List, ListIcon, ListItem } from "@chakra-ui/react";
 import { NavLink } from "react-router-dom";
 
 export default function Sidebar() {
   return (
     <List color="white" fontSize={"1.2em"} spacing={4}>
       <ListItem>
-        <NavLink to="/">Dashboard</NavLink>
+        <NavLink to="/">
+          <ListIcon as={CalendarIcon} color={"white"} />
+          Dashboard
+        </NavLink>
       </ListItem>{" "}
       <ListItem>
-        <NavLink to="/create">New Task</NavLink>
+        <NavLink to="/create">
+          <ListIcon as={EditIcon} color={"white"} />
+          New Task
+        </NavLink>
       </ListItem>{" "}
       <ListItem>
-        <NavLink to="/profile">Profile</NavLink>
+        <NavLink to="/profile">
+          <ListIcon as={AtSignIcon} color={"white"} />
+          Profile
+        </NavLink>
       </ListItem>
     </List>
   );
