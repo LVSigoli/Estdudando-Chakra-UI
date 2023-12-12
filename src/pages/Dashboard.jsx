4eba7c94de6e0d55e31ec3667bb42a455a234364
Lucas Sigoli <lucasviniciussigoli@gmail.com>
2023-12-12 16:20:26 -0300

    fix: Adjust the size of box components

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -2,7 +2,7 @@ import { Box, SimpleGrid, Text } from "@chakra-ui/react";
 
 export default function Dashboard() {
   return (
-    <SimpleGrid columns={4} spacing={10} minChildWidth={"250px"} p="10px">
+    <SimpleGrid columns={4} spacing={10} minChildWidth={"280px"} p="10px">
       <Box bg={"white"} h={"200px"} border={"1px solid"}>
         <Text color={{ base: "pink", md: "blue", lg: "green" }}>Hello</Text>
       </Box>
